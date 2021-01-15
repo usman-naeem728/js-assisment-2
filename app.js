3a1c1a856d7d62e4ccbd90af7da3b566f4b832f3
usman-naeem728 <76055167+usman-naeem728@users.noreply.github.com>
2021-01-15 16:14:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+// Changing case
+
+// 1
+// var a = prompt("Enter your name")
+// var b = a.toUpperCase()
+// console.log(b)
+
+// 2
+// var a = prompt("Enter your name")
+// var b = a.toLowerCase()
+// console.log(b)
+
+// Strings: measuring length and extracting parts
+
+// 1
+// var a = prompt("enter your mobile model")
+// var b = a.length()
+// console.log(b)
+
+// 2
+// var a = prompt("Enter your name")
+// var b = a.lastIndexOf()
+// console.log(b)
+
+// Strings: finding segments
+
+// 1
+// var a = "pakistan"
+// var b = a.indexOf("n")
+// console.log(b)
+
+// 3
+var a = "The quick brown fox jumps over the lazy dog"
+var b = a.replace(/the/,"1","2")
+console.log(b)
+
+// Strings: finding a character at a location
+
+// 1
+// var a = "pakistan"
+// var b = a.charAt("3")
+// console.log(b)
+
+// Strings: replacing characters
+
+// 1
+// var a = "Hyder"
+// var b = "bad"
+// var c = a + b
+// var d = c.replace(a,"Islam")
+// console.log(d)
+
+// 2
+// var a = "Ali and Sami are best friends. They play cricket and football together"
+// var b = a.replace(/and/,"&")
+// console.log(b)
+
+// Rounding numbers
+
+// 1
+// var a = prompt("enter any number")
+// var b = Math.round(a)
+
+// 2
+
+// Generating random number
+
+// 1
+// var a = prompt("enter your lucky number")
+// var b = Math.random(a)
+
+// 2
+// var a = prompt("choose head or tails")
+// if(a === "head"){
+//     console.log("2")
+// }else{
+//     console.log(1)
+// }
+
+
+
